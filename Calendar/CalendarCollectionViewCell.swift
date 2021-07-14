57926387b2041a3995c29b8350ee211323325e53
co3oing <co3oing@naver.com>
2021-07-14 18:19:16 +0900

    Add Days

--- Calendar/CalendarCollectionViewCell.swift
+++ Calendar/CalendarCollectionViewCell.swift
@@ -8,5 +8,25 @@
 import UIKit
 
 class CalendarCollectionViewCell: UICollectionViewCell {
+    let dayLabel: UILabel = UILabel()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setDay()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CalendarCollectionViewCell {
+    func setDay() {
+        self.addSubview(dayLabel)
+        
+        dayLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        dayLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5).isActive = true
+        dayLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    }
 }
--- Calendar/CalendarViewController.swift
+++ Calendar/CalendarViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class CalendarViewController: UIViewController {
-    private let YearMonthView: UILabel = UILabel()
+    private let YearMonthLabel: UILabel = UILabel()
     private let dayOfTheWeekCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
@@ -22,39 +22,53 @@ class CalendarViewController: UIViewController {
         return collectionView
     }()
     
+    private let currentDate = Date()
+    private let dateFormatter = DateFormatter()
     private let dayOfTheWeek: [String] = ["S", "M", "T", "W", "T", "F", "S"]
+    
+    private let calendar = Calendar.current
+    private var components = DateComponents()
+    private var days: [String] = []
+    private var startDayOfTheWeek = 0
+    private var endDay = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        dateFormatter.dateFormat = "yyyy년 MM월"
+        components.year = calendar.component(.year, from: currentDate)
+        components.month = calendar.component(.month, from: currentDate)
+        components.day = 1
+        setDayInCalendar()
         setLayout()
     }
 }
 
 extension CalendarViewController {
     func setLayout() {
-        view.addSubview(YearMonthView)
+        view.addSubview(YearMonthLabel)
         view.addSubview(dayOfTheWeekCollectionView)
         view.addSubview(calendarCollectionView)
         
-        YearMonthView.translatesAutoresizingMaskIntoConstraints = false
+        YearMonthLabel.translatesAutoresizingMaskIntoConstraints = false
         dayOfTheWeekCollectionView.translatesAutoresizingMaskIntoConstraints = false
         calendarCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
         // titleView
-        YearMonthView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        YearMonthView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        YearMonthView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 16).isActive =  true
-        YearMonthView.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        YearMonthLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        YearMonthLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        YearMonthLabel.trailingAnchor.constraint(equalTo:YearMonthLabel.safeAreaLayoutGuide.trailingAnchor, constant: 16).isActive =  true
+        YearMonthLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
         
-        YearMonthView.text = "2021년 7월"
-        YearMonthView.font = .boldSystemFont(ofSize: 22)
+        
+        YearMonthLabel.text = dateFormatter.string(from: currentDate)
+        YearMonthLabel.font = .boldSystemFont(ofSize: 22)
         
         // dayOfTheWeekTableView
         dayOfTheWeekCollectionView.dataSource = self
         dayOfTheWeekCollectionView.delegate = self
         
-        dayOfTheWeekCollectionView.topAnchor.constraint(equalTo: YearMonthView.bottomAnchor).isActive = true
+        dayOfTheWeekCollectionView.topAnchor.constraint(equalTo: YearMonthLabel.bottomAnchor).isActive = true
         dayOfTheWeekCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         dayOfTheWeekCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         dayOfTheWeekCollectionView.heightAnchor.constraint(equalToConstant: 30).isActive = true
@@ -81,7 +95,7 @@ extension CalendarViewController: UICollectionViewDataSource {
         if collectionView == dayOfTheWeekCollectionView {
             return 7
         } else if collectionView == calendarCollectionView {
-            return 35
+            return days.count
         }
         return 0
     }
@@ -95,6 +109,7 @@ extension CalendarViewController: UICollectionViewDataSource {
             let cell = calendarCollectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CalendarCollectionViewCell
             cell.layer.borderWidth = 0.3
             cell.layer.borderColor = UIColor.label.cgColor
+            cell.dayLabel.text = days[indexPath.row]
             return cell
         }
         return UICollectionViewCell()
@@ -123,5 +138,26 @@ extension CalendarViewController: UICollectionViewDelegateFlowLayout {
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
+    }
+}
+
+extension CalendarViewController {
+    func setDayInCalendar() {
+        let firstDay = calendar.date(from: components)!
+        let firstDayOfTheWeek = calendar.component(.weekday, from: firstDay)
+        
+        endDay = calendar.range(of: .day, in: .month, for: firstDay)!.count
+        startDayOfTheWeek = 2-firstDayOfTheWeek
+        
+//        self.YearMonthLabel.text = dateFormatter.string(from: firstDay)
+        
+        days.removeAll()
+        for day in startDayOfTheWeek...endDay {
+            if day < 1 {
+                days.append("")
+            } else {
+                days.append("\(day)")
+            }
+        }
     }
 }
